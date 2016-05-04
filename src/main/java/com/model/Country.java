ba15005d9e861153c3040a9cd13316216cf1995b
Rodion Smakota <rsmakota@gmail.com>
2016-05-04 18:57:44 +0300

    Entity

--- src/main/java/com/model/Country.java
+++ src/main/java/com/model/Country.java
@@ -3,7 +3,8 @@ package com.model;
 import javax.persistence.*;
 import java.io.Serializable;
 
-@Entity(name = "countries")
+@Entity
+@Table(name = "countries", schema = "public", catalog = "ortofit")
 public class Country implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,6 +20,12 @@ public class Country implements Serializable{
     @Column()
     private int length;
 
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+
+    @Id
+    @Column(name = "id")
     public Long getId() {
         return id;
     }
@@ -27,6 +34,8 @@ public class Country implements Serializable{
         this.id = id;
     }
 
+    @Basic
+    @Column(name = "name")
     public String getName() {
         return name;
     }
@@ -35,6 +44,8 @@ public class Country implements Serializable{
         this.name = name;
     }
 
+    @Basic
+    @Column(name = "prefix")
     public String getPrefix() {
         return prefix;
     }
@@ -43,6 +54,8 @@ public class Country implements Serializable{
         this.prefix = prefix;
     }
 
+    @Basic
+    @Column(name = "pattern")
     public String getPattern() {
         return pattern;
     }
@@ -51,6 +64,8 @@ public class Country implements Serializable{
         this.pattern = pattern;
     }
 
+    @Basic
+    @Column(name = "iso2")
     public String getIso2() {
         return iso2;
     }
@@ -59,11 +74,41 @@ public class Country implements Serializable{
         this.iso2 = iso2;
     }
 
+    @Basic
+    @Column(name = "length")
     public int getLength() {
         return length;
     }
 
     public void setLength(int length) {
         this.length = length;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Country country = (Country) o;
+
+        if (length != country.length) return false;
+        if (id != null ? !id.equals(country.id) : country.id != null) return false;
+        if (name != null ? !name.equals(country.name) : country.name != null) return false;
+        if (prefix != null ? !prefix.equals(country.prefix) : country.prefix != null) return false;
+        if (pattern != null ? !pattern.equals(country.pattern) : country.pattern != null) return false;
+        if (iso2 != null ? !iso2.equals(country.iso2) : country.iso2 != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (prefix != null ? prefix.hashCode() : 0);
+        result = 31 * result + (pattern != null ? pattern.hashCode() : 0);
+        result = 31 * result + (iso2 != null ? iso2.hashCode() : 0);
+        result = 31 * result + length;
+        return result;
     }
 }
